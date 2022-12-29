12456bd6266c3eccbd6d4215b8bb19b5fc00f463
ghdcksgml1 <ghdcksgml2@naver.com>
2022-12-29 18:46:08 +0900

    Kotlin Study - 1일차
    - 코틀린 실습 환경 구축
    - 코틀린 패키지에 대해 알아봄.

---
+++ 1일차/KotlinProgramming/src/chap02/section1/defaultPackage.kt
@@ -0,0 +1,14 @@
+package chap02.section1
+
+import kotlin.math.PI
+import kotlin.math.abs
+
+fun main() {
+    val intro: String = "안녕하세요!" // String 자료형 선언
+    val num: Int = 20 // Int 자료형 선언
+
+    println(PI)
+    println(abs(-12.6))
+
+    println("intro: $intro, num: $num")
+}
---
+++ 1일차/MainArgs.kt
@@ -0,0 +1,5 @@
+fun main(args: Array<String>) {
+    println(args[0]) // 외부의 첫 번째 인자
+    println(args[1]) // 외부의 두 번째 인자
+    println(args[2]) // 외부의 세 번째 인자
+}
