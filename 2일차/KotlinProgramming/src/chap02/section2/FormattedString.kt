bc8f3fe8e6fcfacca49df7c8579fe00f1f7a294f
ghdcksgml1 <ghdcksgml2@naver.com>
2022-12-30 16:58:37 +0900

    Kotlin Study - 2일차

---
+++ 2일차/KotlinProgramming/src/chap02/section2/FormattedString.kt
@@ -0,0 +1,11 @@
+package chap02.section2
+
+fun main() {
+    val num = 10
+    val formattedString = """
+        var a = 6
+        var b = "Kotlin"
+        println(a + num)
+        """
+    println(formattedString)
+}
---
+++ 2일차/KotlinProgramming/src/chap02/section4/XorSwapTest.kt
@@ -0,0 +1,10 @@
+package chap02.section4
+
+fun main() {
+    var number1 = 12
+    var number2 = 25
+
+    number1 = number1 xor number2
+    number2 = number1 xor number2
+    number1 = number1 xor number2
+}
